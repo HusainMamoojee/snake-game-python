7fb79bfff4c6bedb2b65ccd4825bafc551eee18d
husain <mamooghusain@gmail.com>
2026-03-25 17:19:01 +0200

    Initial commit: Snake game with food

---
+++ main.py
@@ -0,0 +1,57 @@
+import time
+from turtle import Screen
+from scoreboard import Scoreboard
+from food import Food
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("#1a1a2e")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+snake.create_snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+def start_game():
+    snake.reset()
+    scoreboard.reset()
+    food.refresh()
+
+    game_on = True
+
+    while game_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+        # collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend_snake()
+            scoreboard.increase_score()
+
+        # collision with wall
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 \
+        or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+            game_on = False
+
+        # collision with tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_on = False
+
+    scoreboard.game_over()
+    screen.onkey(start_game, "space")# bind restart after game over
+
+
+start_game()
+screen.exitonclick()
